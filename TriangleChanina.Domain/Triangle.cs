e215b9d20aab956ce0ad800cac48282fe1c52391
Petr Kuderov <pkuderov@gmail.com>
2016-01-29 02:45:11 +0300

    + test cases

--- TriangleChanina.Domain/Triangle.cs
+++ TriangleChanina.Domain/Triangle.cs
@@ -1,13 +1,15 @@
-﻿namespace TriangleChanina.Domain
+﻿using System;
+
+namespace TriangleChanina.Domain
 {
 	public static class Triangle
 	{
 		public static double GetArea(double x, double y, double z)
 		{
 			EnsureTiangleExists(x, y, z);
-
+			
 			var s = (x + y + z) / 2.0;
-			return s;
+			return Math.Sqrt(s * (s - x) * (s - y) * (s - z));
 		}
 
 		public static void EnsureTiangleExists(double x, double y, double z)
@@ -15,8 +17,13 @@
 			if (!(x > 0 && y > 0 && z > 0))
 				throw new TriangleDoesntExistException(x, y, z);
 
-			if (x >= z - y || y >= x - z || z >= y - x)
+			if ((x <= z - y && y <= z - x)
+				|| (y <= x - z && z <= x - y)
+				|| (z <= y - x && x <= y - z)
+			)
+			{
 				throw new TriangleDoesntExistException(x, y, z);
+			}
 		}
 
 	}
--- TriangleChanina.Tests.Unit/TiangleAreaTests.cs
+++ TriangleChanina.Tests.Unit/TiangleAreaTests.cs
@@ -7,18 +7,42 @@ namespace TriangleChanina.Tests.Unit
 	[TestFixture]
 	public class TiangleAreaTests
 	{
-		[SetUp]
-		public void SetUp() { }
 
 		[Test]
-		void When_AtLeastOneSideIsntPositive_Then_ThrowDoesntExist()
+		public void When_AtLeastOneSideIsntPositive_Then_ThrowDoesntExist()
 		{
-			// just shortage
-			Func<TestDelegate, TriangleDoesntExistException> assert = Assert.Throws<TriangleDoesntExistException>;
+			Assert.Throws<TriangleDoesntExistException>(() => Triangle.EnsureTiangleExists(0, 0, 0));
+			Assert.Throws<TriangleDoesntExistException>(() => Triangle.EnsureTiangleExists(-1, 0, 1));
 
-			assert(() => Triangle.EnsureTiangleExists(0, 0, 0));
 			Triangle.EnsureTiangleExists(double.Epsilon, double.Epsilon, double.Epsilon);
+			Triangle.EnsureTiangleExists(4, 2, 3);
 		}
 
+		[Test]
+		public void When_OneSideNotLessThanSumOfTheOthers_Then_ThrowDoesntExist()
+		{
+			Assert.Throws<TriangleDoesntExistException>(() => Triangle.EnsureTiangleExists(1, 2, 3));
+			Assert.Throws<TriangleDoesntExistException>(() => Triangle.EnsureTiangleExists(4, 2, 1));
+
+			Triangle.EnsureTiangleExists(1, 1, double.Epsilon);
+			Triangle.EnsureTiangleExists(double.MaxValue, double.MaxValue, double.MaxValue);
+			Triangle.EnsureTiangleExists(double.MaxValue, double.MaxValue, double.Epsilon);
+		}
+
+		[Test]
+		public void Given_CorrectSides_And_AreaByTwoSidesAndSinus_Then_AreaAndAreaByHeronsFormulaShouldBeEqual()
+		{
+			Assert.AreEqual(Triangle.GetArea(3, 4, 5), 6, double.Epsilon);
+			Assert.AreEqual(Triangle.GetArea(double.MaxValue, double.MaxValue, double.MaxValue), double.PositiveInfinity);
+			Assert.AreEqual(Triangle.GetArea(double.MaxValue, double.Epsilon, double.MaxValue), double.PositiveInfinity);
+			Assert.AreEqual(0.34197, Triangle.GetArea(1.0, 0.9, 0.8), 0.99e-5);
+			
+			Assert.AreEqual(5e+4, Triangle.GetArea(1e+10, 1e+10, 1e-5), 1);
+		}
+
+		private static double FromGrades(double g)
+		{
+			return g * Math.PI / 180.0;
+		}
 	}
 }

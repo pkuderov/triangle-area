5bb7b3d2cb5d57f0b84a39270c3a60cecbcc8396
Petr Kuderov <pkuderov@gmail.com>
2016-01-29 14:28:21 +0300

    fix grammar, test name, separated some test cases

--- TriangleChanina.Tests.Unit/TiangleAreaTests.cs
+++ TriangleChanina.Tests.Unit/TiangleAreaTests.cs
@@ -12,9 +12,6 @@ namespace TriangleChanina.Tests.Unit
 		{
 			Assert.Throws<TriangleDoesntExistException>(() => Triangle.EnsureTiangleExists(0, 0, 0));
 			Assert.Throws<TriangleDoesntExistException>(() => Triangle.EnsureTiangleExists(-1, 0, 1));
-
-			Triangle.EnsureTiangleExists(double.Epsilon, double.Epsilon, double.Epsilon);
-			Triangle.EnsureTiangleExists(4, 2, 3);
 		}
 
 		[Test]
@@ -22,6 +19,13 @@ namespace TriangleChanina.Tests.Unit
 		{
 			Assert.Throws<TriangleDoesntExistException>(() => Triangle.EnsureTiangleExists(1, 2, 3));
 			Assert.Throws<TriangleDoesntExistException>(() => Triangle.EnsureTiangleExists(4, 2, 1));
+		}
+
+		[Test]
+		public void When_AllSidesPositive_And_EverySideNotLessThenSumOfTheOthers_Then_DoesntThrow()
+		{
+			Triangle.EnsureTiangleExists(double.Epsilon, double.Epsilon, double.Epsilon);
+			Triangle.EnsureTiangleExists(4, 2, 3);
 
 			Triangle.EnsureTiangleExists(1, 1, double.Epsilon);
 			Triangle.EnsureTiangleExists(double.MaxValue, double.MaxValue, double.MaxValue);
@@ -29,10 +33,14 @@ namespace TriangleChanina.Tests.Unit
 		}
 
 		[Test]
-		public void Given_CorrectSides_And_AreaByTwoSidesAndSinus_Then_AreaAndAreaByHeronsFormulaShouldBeEqual()
+		public void Given_CorrectSides_And_PrecalculatedArea_Then_PrecalculatedAreaAndAreaByHeronsFormulaShouldBeEqual()
 		{
+			// inf
 			Assert.AreEqual(double.PositiveInfinity, Triangle.GetArea(double.MaxValue, double.MaxValue, double.MaxValue));
-			Assert.AreEqual(double.PositiveInfinity, Triangle.GetArea(double.MaxValue, double.Epsilon, double.MaxValue));
+			Assert.AreEqual(double.PositiveInfinity, Triangle.GetArea(double.MaxValue, double.MaxValue, double.MaxValue));
+			Assert.AreEqual(double.PositiveInfinity, Triangle.GetArea(double.MaxValue, double.MaxValue / 1.5, double.MaxValue / 1.5));
+
+			// 0 by left precision
 			Assert.AreEqual(0, Triangle.GetArea(1, 1, double.Epsilon), double.Epsilon);
 			Assert.AreEqual(0, Triangle.GetArea(1e+10, 1e+10, 1e-10), double.Epsilon);
 
@@ -41,7 +49,7 @@ namespace TriangleChanina.Tests.Unit
 			Assert.AreEqual(6, Triangle.GetArea(5, 4, 3), double.Epsilon);
 			Assert.AreEqual(6, Triangle.GetArea(5, 3, 4), double.Epsilon);
 
-			// calculated in google online calc - low accurancy
+			// calculated in google online calc - low precision
 			Assert.AreEqual(0.34197, Triangle.GetArea(1.0, 0.9, 0.8), 1.0e-6);
 			
 			// s = 7.25, area^2 = 20.20484375, calculated in windows calc
